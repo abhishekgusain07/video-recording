65132b4b50d55df6b5e9145107d37eb2dd273a07
abhishekgusain07 <abhishek.gusain1007fb@gmail.com>
2024-11-10 23:28:39 +0530

    add: learnings

--- src/components/global/folders/index.tsx
+++ src/components/global/folders/index.tsx
@@ -37,8 +37,10 @@ const Folders = ({
     const {data:folders , status} = data as FoldersProps
 
     if(isFetched && folders) {
-        
+        //dispatch folders in redux store.
     }
+
+
     return (
         <div className="flex flex-col gap-4">
             <div className="flex items-center justify-between">
